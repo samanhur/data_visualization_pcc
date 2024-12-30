3f1c6a04ee9754514fe47ba3cdb2d511aef14c6d
samanhur <samanghasemidesk@gmail.com>
2024-12-30 20:57:57 +0330

    chapter16_first_learning_section

---
+++ learning_sections/sitka_highs.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+path = Path("weather_data/sitka_weather_07-2021_simple.csv")
+lines = path.read_text().splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+# Read the header of data
+# print(header_row)
+# for index, column_header in enumerate(header_row):
+    # print(index, column_header)
+
+# Extract dates and high tempearatures.
+dates, highs = [], []
+for row in reader:
+    current_date = datetime.strptime(row[2], "%Y-%m-%d")
+    high = int(row[4])
+    
+    dates.append(current_date)
+    highs.append(high)
+
+# print(highs)
+
+# Plot the high temperatures.
+plt.style.use("seaborn-v0_8-whitegrid")
+fig, ax = plt.subplots()
+ax.plot(dates, highs, color='red')
+
+# Format plot.
+ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+ax.set_xlabel("", fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(labelsize=16)
+
+plt.show()
